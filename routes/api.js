adaea515f662478a8669ae439596008b00fa866a
ginggonzalez03 <ginggonzalez3@gmail.com>
2017-05-04 23:16:43 -0700

    fix: API routes modified
    The API endpoints are modified because some of the paths are supposed to be
    query parameters. In this commit, those parameters are now usable.

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/oppcost/:quantitya/:quantityb', function(req, res, next) {
-	var oppcosta = req.params.quantityb / req.params.quantitya;
-	var oppcostb = req.params.quantitya / req.params.quantityb;
+
+/*
+ * TODO: Separate these functions
+ */
+
+/**
+ * Gets opportunity costs for two different items to get
+ * the opportunity cost for going for the first item
+ * @param  {int} qA first option and the one that the function will calculate the opp cost for
+ * @param  {int} qB second option
+ * @return {dictionary}    Opportunity Cost for qA
+ */
+function oppCostForFirstItem(qA, qB)
+{
+	return qB/qA;
+}
+
+function absoluteAdvantage(q1A, q1B, q2A, q2B)
+{
+	var absAdv;
+
+	if(q2A * q1B > q1A * q2B)
+	{
+		absAdv = q2A * q1B;
+	}
+	else
+	{
+		absAdv = q1A * q2B;
+	}
+	return absAdv;
+}
+
+/*
+ * Gets opportunity costs for two different items
+ */
+router.get('/oppcost', function(req, res, next) {
+	var oppcosta = oppCostForFirstItem(req.query.quantitya,req.query.quantityb);
+	var oppcostb = oppCostForFirstItem(req.query.quantityb,req.query.quantitya);
 	res.send({oppcosta: oppcosta, oppcostb : oppcostb});
+});
+
+/*
+ * Gets two opportunity costs for two items for two doers to get which one has
+ * the absolute advantage/comparative advantage
+ */
+router.get('/absadv', function(req, res, next) {
+	var absAdv = absoluteAdvantage(
+		req.query.quantity1a,
+		req.query.quantity1b,
+		req.query.quantity2a,
+		req.query.quantity2b);
+	res.send({absAdv: absAdv});
 });
 
 module.exports = router;
